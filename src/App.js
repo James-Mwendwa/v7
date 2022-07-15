6911dd60fedf176001fc70de32ab825d19f1983a
James Mwendwa <mwendwajames32@gmail.com>
2022-07-15 19:53:24 +0300

    ESLint with jsx

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React from 'react';
+import ReactDOM from "react-dom";
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Moon",
-      animal: "Cat",
-      breed: "whatever",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havana",
-    }),
-    React.createElement(Pet, {
-      name: "Dirty",
-      animal: "Pig",
-      breed: "White",
-    })
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
   );
-};
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
