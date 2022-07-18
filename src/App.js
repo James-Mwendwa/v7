bcdf039cdaa7c06a4ee1f1423507a0b4d01269d8
James Mwendwa <mwendwajames32@gmail.com>
2022-07-18 16:23:11 +0300

    Core Concepts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import SearchParams from './SearchParams';
-
+import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
-}
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
